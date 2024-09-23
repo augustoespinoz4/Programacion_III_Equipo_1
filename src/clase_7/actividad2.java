79aa68c9b645bf9696a8012901694c3b5af36512
NicolasAlfonsin <128539415+NicolasAlfonsin@users.noreply.github.com>
2024-09-22 23:47:19 -0300

    Update actividad2.java

--- src/clase_7/actividad2.java
+++ src/clase_7/actividad2.java
@@ -1,128 +1,100 @@
 package clase_7;
 
-import java.util.*;
+import java.util.Scanner;
 
-class actividad2{
-    static final int MAXN = 100;
-    // Infinite value for array
-    static int INF = 800000000;
+public class actividad2 {
+    final static int INF = Integer.MAX_VALUE;
 
-    static int [][]dis = new int[MAXN][MAXN];
-    static int [][]Next = new int[MAXN][MAXN];
-    // Initializing the distance and
-// Next array 
-    static void initialise(int V,
-                           int [][] graph)
-    {
-        for(int i = 0; i < V; i++)
-        {
-            for(int j = 0; j < V; j++)
-            {
-                dis[i][j] = graph[i][j];
+    public static void main(String[] args) {
+        actividad2 floyd = new actividad2();
+        int[][] graph = {
+                {0, 2, INF, 5},
+                {INF, 0, INF, 4},
+                {INF, INF, 0, INF},
+                {INF, INF, 2, INF}
+        };
+        int V = graph.length;
+        floyd.floydWarshall(graph, V);
 
-                // No edge between node
-                // i and j
-                if (graph[i][j] == INF)
-                    Next[i][j] = -1;
-                else
-                    Next[i][j] = j;
+        // Ejemplo de cómo obtener el camino más corto entre dos nodos
+        System.out.println("Inserte un nodo de inicio: ");
+        Scanner entrada = new Scanner(System.in);
+        int desde = entrada.nextInt();
+        System.out.println("Inserte un nodo hasta el que quiera llegar: ");
+        entrada = new Scanner(System.in);
+        int hasta = entrada.nextInt();
+
+        floyd.imprimirCamino(desde, hasta, graph);
+    }
+
+    void floydWarshall(int[][] grafo, int V) {
+        int[][] dist = new int[V][V];
+        int[][] predecesor = new int[V][V];
+
+        // Inicializar la matriz de distancias y predecesores
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                dist[i][j] = grafo[i][j];
+                predecesor[i][j] = (grafo[i][j] != INF && i != j) ? i : -1;
             }
         }
-    }
-    // Function construct the shortest
-// path between u and v 
-    static Vector<Integer> constructPath(int u, int v)
-    {
-        // If there's no path between
-        // node u and v, simply return
-        // an empty array
-        if (Next[u][v] == -1)
-            return null;
 
-        // Storing the path in a vector
-        Vector<Integer> path = new Vector<Integer>();
-        path.add(u);
-
-        while (u != v)
-        {
-            u = Next[u][v];
-            path.add(u);
-        }
-        return path;
-    }
-
-    // Standard Floyd Warshall Algorithm
-// with little modification Now if we find 
-// that dis[i][j] > dis[i][k] + dis[k][j] 
-// then we modify next[i][j] = next[i][k] 
-    static void floydWarshall(int V)
-    {
-        for(int k = 0; k < V; k++)
-        {
-            for(int i = 0; i < V; i++)
-            {
-                for(int j = 0; j < V; j++)
-                {
-
-                    // We cannot travel through
-                    // edge that doesn't exist
-                    if (dis[i][k] == INF ||
-                            dis[k][j] == INF)
-                        continue;
-
-                    if (dis[i][j] > dis[i][k] +
-                            dis[k][j])
-                    {
-                        dis[i][j] = dis[i][k] +
-                                dis[k][j];
-                        Next[i][j] = Next[i][k];
+        // Aplicar el algoritmo de Floyd-Warshall
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF &&
+                            dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        predecesor[i][j] = predecesor[k][j]; // Actualizar predecesor
                     }
                 }
             }
         }
+
+        imprimir(dist, V);
+        this.predecesor = predecesor; // Guardar predecesores en un atributo de clase
     }
 
-    // Print the shortest path
-    static void printPath(Vector<Integer> path)
-    {
-        int n = path.size();
-        for(int i = 0; i < n - 1; i++)
-            System.out.print(path.get(i) + " -> ");
-        System.out.print(path.get(n - 1) + "\n");
+    int[][] predecesor; // Para almacenar los predecesores
+
+    void imprimir(int[][] dist, int V) {
+        System.out.println("Matriz de distancias más cortas entre cada par de vértices:");
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j] + "   ");
+            }
+            System.out.println();
+        }
     }
 
-    // Driver code
-    public static void main(String[] args)
-    {
-        int V = 4;
-        int [][] graph = { { 0, 3, INF, 7 },
-                { 8, 0, 2, INF },
-                { 5, INF, 0, 1 },
-                { 2, INF, INF, 0 } };
+    void imprimirCamino(int start, int end, int[][]graph) {
+        System.out.print("Camino más corto desde " + start + " hasta " + end + ": ");
+        if (predecesor[start][end] == -1) {
+            System.out.println("No hay camino.");
+            return;
+        }
 
-        // Function to initialise the
-        // distance and Next array
-        initialise(V, graph);
+        StringBuilder camino = new StringBuilder();
+        int suma = 0;
+        int at = end;
 
-        // Calling Floyd Warshall Algorithm,
-        // this will update the shortest
-        // distance as well as Next array
-        floydWarshall(V);
-        Vector<Integer> path;
-
-        // Path from node 1 to 3
-        System.out.print("Shortest path from 1 to 3: ");
-        path = constructPath(1, 3);
-        printPath(path);
-
-        // Path from node 0 to 2
-        System.out.print("Shortest path from 0 to 2: ");
-        path = constructPath(0, 2);
-        printPath(path);
-
-        // Path from node 3 to 2
-        System.out.print("Shortest path from 3 to 2: ");
-        path = constructPath(3, 2);
-        printPath(path);
+        // Reconstruir el camino
+        while (at != start) {
+            if (at == -1) {
+                System.out.println("No hay camino.");
+                return;
+            }
+            camino.insert(0, at + " -> ");
+            int prev = predecesor[start][at];
+            suma += (prev != -1) ? graph[prev][at] : 0; // Sumar el peso de la arista
+            at = prev;
+        }
+        camino.insert(0, start + " -> "); // Agregar el nodo de inicio
+        System.out.println(camino);
+        System.out.println("Suma de los valores del camino: " + suma);
     }
-} 
+}
